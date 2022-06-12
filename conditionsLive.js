1a184c52a68434694f12a4566082aa91c56b7781
PeterQHo <pho@nyc.yearup.org>
2022-06-12 13:37:06 -0700

    Adds comments

--- conditionsLive.js
+++ conditionsLive.js
@@ -32,7 +32,7 @@ if (randomNumber >= 0.5) {
 // constant variable value is Monday
 const dayOfWeek = 'Monday';
 
-// Using if statement and strict equality for dayOfWeek 
+// Using if statement and strict equality for dayOfWeek and will log into the console 
 if (dayOfWeek === 'Monday') {
 
     console.log("This is Monday");
@@ -49,16 +49,21 @@ if (dayOfWeek === 'Monday') {
 */
 
 /*
+
+// creating a promt to enter age 
 const age = prompt('Enter An Age to Drive')
 
+// using if statement for ages less than 5 and logging the following string into the console
 if (age < 5) {
 
     console.log("You are less that 5! Cmon bro.");
 
+// using if statement for ages less than 10 and logging the following string into the console
 } else if (age < 10) {
 
     console.log("Still to young");
 
+// using if statement for ages less than or equal to 16 and logging the following string into the console
 } else if (age >= 16) {
 
     console.log("You are able to drive");
@@ -67,16 +72,19 @@ if (age < 5) {
 
 // Else --- All Condition in the logic failed 
 
+// Using else statement if condition do not meet the statements log the following string in the console
 else {
 
     console.log("Last Resort If all Conditions Fails");
 }
 */
 /*
+
+// Declaring a variable 
 const expr = "Papayas";
-
+// Using switch statement to pass on the expression
 switch (expr) {
-
+// Creating case statements for fruits and using break function to break out of the execution
     case 'Oranges':
         console.log("Oranges are $0.59 a pound");
         break;
@@ -86,59 +94,64 @@ switch (expr) {
     case 'Papayas':
         console.log("Hello Papayas");
         break;
+    // Default statement if none of the parameter matches the case statment log the string in the console
     default:
-        console.log("Sorry, we are out of you asked item.");
+        console.log("Sorry, we are out of your asked item.");
 
 }
 */
 /*
+// Creating a prompt
 const password = prompt("Please Enter A New Password");
-
+// If statement with condtion of password.length greater than or equal to 6 characters
 if (password.length >= 6) {
 
-    // Nested Condition
+    // Nested Condition to look for a occurrence in the string and return -1 if value is not found
     if (password.indexOf(' ') === -1) {
-
+        // If condtion are met log the string in the console
         console.log('Valid password');
 
     } else {
-
+        // Else statement log in console if password.indexOf is not met
         console.log("Password Can't Have Spaces");
 
     }
 
 } else {
 
-
+    // Else statement log in console if password.length is not met
     console.log("Password is Too Short");
 
 }
 */
 /*
+
+// Creating a function with three different outputs 
 function singSong() {
     console.log("DO");
     console.log("RE");
     console.log("MI");
 }
-
+// log function into console
 singSong()
 singSong()
 singSong()
 */
 
+// creating function name of greet with argument parameter of firstName
 function greet(firstName) {
-// template literal
-    console.log(`Hey There, ${firstName}`)
+//  log in the sting and using template literal for the firstName  
+    console.log(`Hey There, ${firstName}`);
 
 }
-
+// Calling the function name greet to Peter
 greet('Peter')
 
 function greet1(firstName, lastName) {
-
-    console.log(`Hey There, ${firstName} ${lastName[0]}.`)
+// log the string and using template literal for firstName and the first character of lastName
+    console.log(`Hey There, ${firstName} ${lastName[0]}.`);
     
 
 }
-
+// Calling the function name of greet1 to Boomer, Sooner
 greet1('Boomer','Sooner');
